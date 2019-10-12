025286c161f48bbd0b2ab9e81dcdbad18d23bfd0
Thomas Wade <gigatech55@gmail.com>
2019-10-12 22:43:46 +1030

    Add keypad logic

--- Arduino Sketches/module_keypad/module.c
+++ Arduino Sketches/module_keypad/module.c
@@ -43,6 +43,25 @@
 #define LCD_BACKLIGHT_PORT PORTC
 #define LCD_BACKLIGHT_PIN 3
 
+// Define some macros for the keypad pins
+#define KEY_COL1_PORT PORTD
+#define KEY_COL1_PIN 1
+#define KEY_COL2_PORT PORTD
+#define KEY_COL2_PIN 2
+#define KEY_COL3_PORT PORTD
+#define KEY_COL3_PIN 3
+#define KEY_ROW1_PORT PORTD
+#define KEY_ROW1_PIN 4
+#define KEY_ROW2_PORT PORTD
+#define KEY_ROW2_PIN 5
+#define KEY_ROW3_PORT PORTD
+#define KEY_ROW3_PIN 6
+#define KEY_ROW4_PORT PORTD
+#define KEY_ROW4_PIN 7
+
+// Define some macros for type shortcuts
+#define byte unsigned char
+
 // Function prototypes
 void init();
 void onDisarm();
@@ -53,18 +72,44 @@ void generateSequence();
 unsigned char getRandom();
 
 // Consts
-const unsigned char keypadChars[] = {'0','1','2','3','4','5','6','7','8','9','*','#'};
-const unsigned char sequenceLength = 7;
+const char keypadChars[] = {'0','1','2','3','4','5','6','7','8','9','*','#'};
+const byte sequenceLength = 7;
 
 // State variables
 bool running = true;
 unsigned short ptrRnd = 0;
-unsigned char sequence[sequenceLength];
-unsigned char sequenceIndex;
+char sequence[sequenceLength];
+byte sequenceIndex;
 
 // Interrupt states
 bool disarmPinLast;
 bool lowPowerPinLast;
+
+// Keypad state
+bool keypadState[4][3] =
+{
+    { // Row 1
+        false, // Button 1
+        false, // Button 2
+        false  // Button 3
+    },
+    { // Row 2
+        false, // Button 4
+        false, // Button 5
+        false  // Button 6
+    },
+    { // Row 3
+        false, // Button 7
+        false, // Button 8
+        false  // Button 9
+    },
+    { // Row 4
+        false, // Button 10
+        false, // Button 11
+        false  // Button 12
+    }
+}
+byte numKeysPressed = 0;
 
 void init()
 {
@@ -82,7 +127,7 @@ int main() {
     init();
 
     // Enter the Main Run Loop (MRL) -- Advanced military tech right here
-    while (true)
+    while (running)
     {
         // Check if we should disarm or enter low power mode
         bool disarmPin = (DISARM_PORT >> DISARM_PIN) & 1;
@@ -96,7 +141,21 @@ int main() {
         lowPowerPinLast = lowPowerPin;
         disarmPinLast = disarmPin;
 
-        // TODO: Update display
+        // Update the keypad state and check it against the current sequence
+        updateKeypadState();
+        if (getNumKeysPressed == 1 && getKeypadKey(sequence[sequenceIndex]))
+        {
+            // Move to the next item in the sequence
+            sequenceIndex++;
+
+            // Check if we have finished the sequence
+            if (sequenceIndex == sequenceLength)
+            {
+                // TODO: Mark as defused
+            }
+
+            // TODO: Update the LCD
+        }
     }
 }
 
@@ -141,9 +200,9 @@ void writeByteMSB(unsigned char byte)
 }
 
 // Read a random value from randomvals.h, wrap when reaching the end of the table.
-unsigned char getRandom()
+byte getRandom()
 {
-    unsigned char rnd = pgm_read_byte(&(random0[ptrRnd++]));
+    byte rnd = pgm_read_byte(&(random0[ptrRnd++]));
 
     if (ptrRnd == sizeof(random0)) ptrRnd = 0;
 
@@ -157,5 +216,80 @@ void generateSequence()
     {
         // Get a random keypad character and append it
         sequence[i] = keypadChars[getRandom() % 13];
+    }
+}
+
+// Probes the keypad and updates the stored state of each key
+void updateKeypadState()
+{
+    // 1, 2, 3
+    keypadState[0][0] = readKeypadKey(KEY_ROW1_PORT, KEY_ROW1_PIN, KEY_COL1_PIN);
+    keypadState[0][1] = readKeypadKey(KEY_ROW1_PORT, KEY_ROW1_PIN, KEY_COL2_PIN);
+    keypadState[0][2] = readKeypadKey(KEY_ROW1_PORT, KEY_ROW1_PIN, KEY_COL3_PIN);
+
+    // 4, 5, 6
+    keypadState[1][0] = readKeypadKey(KEY_ROW2_PORT, KEY_ROW2_PIN, KEY_COL1_PIN);
+    keypadState[1][1] = readKeypadKey(KEY_ROW2_PORT, KEY_ROW2_PIN, KEY_COL2_PIN);
+    keypadState[1][2] = readKeypadKey(KEY_ROW2_PORT, KEY_ROW2_PIN, KEY_COL3_PIN);
+
+    // 7, 8, 9
+    keypadState[2][0] = readKeypadKey(KEY_ROW3_PORT, KEY_ROW3_PIN, KEY_COL1_PIN);
+    keypadState[2][1] = readKeypadKey(KEY_ROW3_PORT, KEY_ROW3_PIN, KEY_COL2_PIN);
+    keypadState[2][2] = readKeypadKey(KEY_ROW3_PORT, KEY_ROW3_PIN, KEY_COL3_PIN);
+
+    // *, 0, #
+    keypadState[3][0] = readKeypadKey(KEY_ROW4_PORT, KEY_ROW4_PIN, KEY_COL1_PIN);
+    keypadState[3][1] = readKeypadKey(KEY_ROW4_PORT, KEY_ROW4_PIN, KEY_COL2_PIN);
+    keypadState[3][2] = readKeypadKey(KEY_ROW4_PORT, KEY_ROW4_PIN, KEY_COL3_PIN);
+}
+
+// Probes an individual keypad key on the given port, row, and column
+bool readKeypadKey(unsigned char port, unsigned char row_pin, unsigned char column_pin)
+{
+    // Write the row high
+    port |= (1 << row_pin);
+
+    // Get the pin state
+    bool state = ((port >> pin) & 1);
+
+    // Write the row low
+    port &= ~(1 << row_pin);
+
+    // Return the state we got
+    return state;
+}
+
+// Gets the number of keypad keys currently pressed
+byte getNumKeysPressed()
+{
+    byte num;
+
+    for (byte y = 0; y < 4; y++)
+    {
+        for (byte x = 0; x < 3; x++)
+        {
+            num += keypadState[y][x];
+        }
+    }
+}
+
+// Gets the key state for the given keypad character
+bool getKeypadKey(char key)
+{
+    // Sanity check, make sure it's only #, *, or 0-9
+    if ((key != 35 || key != 42) && (key < 48 || key > 57)) return false;
+
+    switch (key)
+    {
+        case '*': // #
+            return keypadState[3][0];
+        case '0': // 0
+            return keypadState[3][1];
+        case '#': // *
+            return keypadState[3][2];
+        default: // 0-9
+            // Subtract 43 from the key to compensate for ASCII and zero
+            key -= 43;
+            return keypadState[key / 3][key % 3];
     }
 }
